e4a941f8d4f1d32df4edf4e01eead62249be2b4a
raj37 <rk17081990@gmail.com>
2022-10-07 08:42:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+1. create env
+```bash
+conda create -n wineq python=3.7 -y
+```
+2. activate env
+```bash
+conda activate wineq 
+```
+3. create requirements.txt file
+4. install the requirements
+```bash
+pip install -r requirements.txt
+```
+5. download the data from
+```bash
+https://drive.google.com/drive/folders/18zqQiCJVgF7uzXgfbIJ-04zgz1ItNfF5?usp=sharing
+```
+6. git init
+7. dvc init
+8. dvc add data_given/winequality.csv
---
+++ template.py
@@ -0,0 +1,31 @@
+import os
+
+# directory structure we want to setup for ML project
+dirs = [
+    os.path.join("data", "raw"),
+    os.path.join("data", "processed"),
+    "notebooks",
+    "saved_models",
+    "src"
+]
+
+# loop-in and create above directory structure we agreed upon for ML project
+for dir_ in dirs:
+    os.makedirs(dir_, exist_ok=True)
+    # create a .gitkeep file in every directory we will create using for loop
+    # This will be helpful to track empty directories in Git
+    with open(os.path.join(dir_, ".gitkeep"), "w") as f:
+        pass
+# .gitignore is also used to list files that should be ignored by Git when looking for untracked files.
+files = [
+    "dvc.yml",
+    "params.yml",
+    ".gitignore",
+    os.path.join("src", "__init__.py")
+]
+
+for file_ in files:
+    with open(file_, "w") as f:
+        pass
+
+# data_given dir is nothing but remote repo for input data for model build
